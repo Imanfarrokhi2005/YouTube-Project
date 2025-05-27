4cb998a21bb1b6f4987065e9b5a4618a856bbd0a
Iman <imanfarrokhii1383@gmail.com>
2025-05-27 18:59:14 +0330

    Storing video data and using it to auto generate page

---
+++ data/videos.js
@@ -0,0 +1,20 @@
+export const videos = [
+  {
+    profile: {
+      profileLink: "https://www.youtube.com/c/mkbhd",
+      profileImage: "images/channels/channel-1.jpeg",
+      profileName: "Marques Brownlee",
+      subscribesNumber: "15M",
+    },
+    video: {
+      videoTitle: "Talking Tech and AI with Google CEO Sundar Pichai!",
+      videoLink: "https://www.youtube.com/watch?v=n2RNcPRtAiY",
+      tumbnailImage: "images/thumbnails/thumbnail-1.webp",
+      videoTime: "14:20",
+      videoStats: {
+        view: "3.4M",
+        date: "6 months",
+      },
+    },
+  },
+];
---
+++ youtube.js
@@ -0,0 +1,48 @@
+import { videos } from "./data/videos.js";
+
+renderPage();
+
+function renderPage() {
+  let pageHTHML = "";
+
+  videos.forEach((video) => {
+    pageHTHML += `
+      <div class="video">
+        <div class="video-preview">
+          <a href=${video.video.videoLink} target="_blank">
+            <img src=${video.video.tumbnailImage} class="thumbnail">
+          </a>
+          <div class="video-time">${video.video.videoTime}</div>
+        </div>
+        <div class="video-info-grid">
+          <div class="channel-profile">
+            <a href=${video.profile.profileLink} target="_blank"><img src=${video.profile.profileImage}
+                class="channel-picture">
+            </a>
+            <div class="profile-demo">
+              <div><img src=${video.profile.profileImage}></div>
+              <div class="info-demo">
+                <div>${video.profile.profileName}</div>
+                <div><span>${video.profile.subscribesNumber} subscribers</span></div>
+              </div>
+            </div>
+          </div>
+          <div class="video-infos">
+            <a href=${video.video.videoLink} target="_blank"
+              style="text-decoration: none; color: black;"">
+              <p class=" video-title">
+              ${video.video.videoTitle}</p>
+            </a>
+            <a href=${video.profile.profileLink} target="_blank" style="text-decoration: none;
+            color: black;">
+              <p class="video-author">${video.profile.profileName}</p>
+            </a>
+            <p class="video-stats">${video.video.videoStats.view} views &#183; ${video.video.videoStats.date} ago</p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  document.querySelector(".js-video-grid").innerHTML = pageHTHML;
+}
